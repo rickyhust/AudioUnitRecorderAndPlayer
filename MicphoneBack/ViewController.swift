faf4234948824a93ac61a715e7f0bc0aed70819e
rickyhome <rickyhust@outlook.com>
2017-01-17 20:39:27 +0800

    remove test code

--- MicphoneBack/ViewController.swift
+++ MicphoneBack/ViewController.swift
@@ -13,23 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //Test C library pointer conversion
-        struct Context {
-            var city = "Tokyo"
-        }
-        
-        var context: Context = Context()
-        let rawPtr = UnsafeMutableRawPointer(&context)
-        let b = rawPtr as? UnsafeMutableRawPointer
-        let opaquePtr = OpaquePointer(rawPtr)
-        let contextPtr = UnsafeMutablePointer<Context>(opaquePtr)
-        
-        var s = context.city // "Tokyo"
-        contextPtr.pointee.city = "New York"
-        s = context.city // "New York"
-        
-        print(s)
     }
 
     override func didReceiveMemoryWarning() {
