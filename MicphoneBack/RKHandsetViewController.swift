e6acd489d25d5a9bed3c26e41d248a6e2e888341
ricky <apple@rickydeiMac.local>
2017-01-17 11:30:37 +0800

    add new view controller

---
+++ MicphoneBack/RKHandsetViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RKHandsetViewController.swift
+//  MicphoneBack
+//
+//  Created by apple on 17/1/17.
+//  Copyright © 2017年 Ricky. All rights reserved.
+//
+
+import UIKit
+
+class RKHandsetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
